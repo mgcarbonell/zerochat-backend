c18b398d0b12b999f5eddc76b4b0ee53800eaae5
mgcarbonell <mgcarbonell@gmail.com>
2021-03-23 16:56:22 -0700

    CORS error fix

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 // imports
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
-const session = require('express-session')
-const morgan = require('morgan')
-const http = require('http')
-const routes = require('./routes')
-const passport = require('./passport')
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const session = require("express-session")
+const morgan = require("morgan")
+const http = require("http")
+const routes = require("./routes")
+const passport = require("./passport")
 const {
   addUsers,
   removeUser,
   getUser,
-  usersInNode
-              } = require('./roomManagement.js') 
+  usersInNode,
+} = require("./roomManagement.js")
 
 const port = process.env.PORT || 4000
 const app = express()
 
-
-
 // middleware - server logging
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 
 // middleware - JSON parsing
 app.use(express.json())
@@ -29,64 +27,83 @@ app.use(express.json())
 const corsOptions = {
   // from which URLs do we want to accept requests
   origin: [process.env.CLIENT_URL],
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: ['Content-Type'],
   credentials: true, // allow the session cookie to be sent to and from the client
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 204,
 }
 
 app.use(cors(corsOptions))
 
 const server = http.createServer(app)
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   cors: {
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
-    credentials: true
-  }
-});
+    credentials: true,
+  },
+})
 
-app.use(session({
-  // session is stored in the DB
-  secret: process.env.SESSION_SECRET,
-  resave: false, // will not resave sessions
-  saveUninitialized: false, // only create a session when a property is added to the session
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24
-  } 
-}))
+app.use(
+  session({
+    // session is stored in the DB
+    secret: process.env.SESSION_SECRET,
+    resave: false, // will not resave sessions
+    saveUninitialized: false, // only create a session when a property is added to the session
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+)
 
 // middleware - passport config
 app.use(passport.initialize())
 app.use(passport.session())
 
 // middleware - API routes
-app.use('/api/v1/auth', routes.auth)
-app.use('/api/v1/users', routes.users)
+app.use("/api/v1/auth", routes.auth)
+app.use("/api/v1/users", routes.users)
 
-io.on('connect', (socket) => {
+io.on("connect", (socket) => {
   // event and a call back
-  socket.on('join', ({ username, node }, callback) => {
+  socket.on("join", ({ username, node }, callback) => {
     // console.log({username, node})
-    const { user } = addUsers({ id: socket.id, username, node });
+    const { user } = addUsers({ id: socket.id, username, node })
     socket.join(user.node)
-    socket.emit('message', { user: 'null.void', text: `${user.username}, connecting to node ${user.node}`});
-    socket.broadcast.to(user.node).emit('message', { user: 'null.void', text: `${user.username} joining from PORT: ${Math.floor(Math.random() * 9999)}`});
-    io.to(user.node).emit('nodeData', { node: user.node, users: usersInNode(user.node) })
-    callback();
+    socket.emit("message", {
+      user: "null.void",
+      text: `${user.username}, connecting to node ${user.node}`,
+    })
+    socket.broadcast
+      .to(user.node)
+      .emit("message", {
+        user: "null.void",
+        text: `${user.username} joining from PORT: ${Math.floor(
+          Math.random() * 9999
+        )}`,
+      })
+    io.to(user.node).emit("nodeData", {
+      node: user.node,
+      users: usersInNode(user.node),
+    })
+    callback()
   })
   // socket takes in an event, and a callback
-  socket.on('sendMessage', (message, callback) => {
+  socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id)
-    io.to(user.node).emit('message', { user: user.username, text: message });
-    callback();
+    io.to(user.node).emit("message", { user: user.username, text: message })
+    callback()
   })
   // event and a call back
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     const user = removeUser(socket.id)
-    if(user){
-      io.to(user.node).emit('message', { user: 'null.void', text: `${user.username} is returning to meatspace.`})
-      io.to(user.node).emit('nodeData', { node: user.node, users: usersInNode(user.node)})
+    if (user) {
+      io.to(user.node).emit("message", {
+        user: "null.void",
+        text: `${user.username} is returning to meatspace.`,
+      })
+      io.to(user.node).emit("nodeData", {
+        node: user.node,
+        users: usersInNode(user.node),
+      })
     }
   })
 })
